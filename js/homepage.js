ba3fce038684194dae576923873a047811cc7fb4
Mark Fisher <ravidfish@gmail.com>
2015-01-14 17:05:04 -0700

    Adding webpage, assignment page, and supporting pages

---
+++ js/homepage.js
@@ -0,0 +1,11 @@
+function mouseOver()
+{
+	document.getElementById("assignments_link").style.color = "grey";
+}
+
+function mouseOut()
+{
+	document.getElementById("assignments_link").style.color = "white";
+
+}
+		
