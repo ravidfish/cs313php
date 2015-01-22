18fcc1e0b9b1316aeab4a38ec00be8edc7e8f9ac
Mark Fisher <ravidfish@gmail.com>
2015-01-22 11:09:09 -0700

    added files for survey assignment

---
+++ cookies.php
@@ -0,0 +1,26 @@
+<?php
+
+sessions_start();
+
+$_SESSION["count"] = 10;
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Sessions</title>
+	</head>
+	<body>
+		<div>
+			This is a page with sessions
+
+<?php 
+
+	
+
+?>			
+			
+		</div>
+	</body>
+</html>
