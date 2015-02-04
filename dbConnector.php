831717c890bc283065a48f15e39a970217fcebaf
Mark Fisher <ravidfish@gmail.com>
2015-02-03 19:59:07 -0700

    still more testing db

--- dbConnector.php
+++ dbConnector.php
@@ -10,11 +10,11 @@ function loadDatabase()
 	$dbName = "dinobabies";
 
     $openShiftVar = getenv('OPENSHIFT_MYSQL_DB_HOST');
-	echo "before if";
+	echo "before if ";
     if ($openShiftVar === null || $openShiftVar == "")
     {
 		// Not in the openshift environment
-   
+		echo "in if "
         require("setLocalDatabaseCredentials.php");
 				
 		$db = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPassword);
@@ -22,7 +22,7 @@ function loadDatabase()
     else 
     { 
         // In the openshift environment
-		echo "in the else";
+		echo "in the else ";
         $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
         $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT'); 
         $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
