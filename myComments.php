8346caef8ce5d25b413b7ed1332b227292d9d0c0
Mark Fisher <ravidfish@gmail.com>
2015-02-09 14:34:30 -0700

    making site more dynamic and fixing search

--- myComments.php
+++ myComments.php
@@ -48,61 +48,13 @@ $db = loadDatabase();
 		<!-- header contents -->
 		<div class="container">
 			<div class="jumbotron">
-				<h1>Dinobabies</h1>            
+				<h1>My Comments</h1>            
 			</div>
 			<div class="row">
 				<div class="col-md-4">
-			
-<?php 
-/*	
-echo "<br />\n<br />\n" . "Comments for item 1:" . "<br />\n<br />\n";
-
-$sql = "SELECT c.date, c.body, u.fName, u.lName from comment c join user u on c.user_id = u.user_id WHERE c.item_id = :item";
-$item = "1";
-$statement = $db->prepare($sql);
-$statement->bindValue(':item', $item, PDO::PARAM_STR);
-$statement->execute();
-
-$count = 0;
-
-while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-{	
-	$count++;
-	echo "Comment Number: " . $count . "<br />\n";
-	echo "Date: " . $row["date"] . "<br />\n";
-	echo "Body: " . $row["body"] . "<br />\n";
-	echo "Authors Name: " . $row["fName"] . " " . $row["lName"] . "<br />\n";
-	echo "<br />\n";
-}
-*/
-?>
-			
-				</div>
-				<div class="col-md-4">
-			
-<?php
-/*
-echo "<br />\n<br />\n" . "All results that best match your query:" . "<br />\n<br />\n";
-$sql = "select i.name, c.colorName, s.size from item i join itemcolorsize ics on i.item_id = ics.item_id join color c on ics.color_id = c.color_id join size s on ics.size_id = s.size_id where i.name = :item";
-$item = "beanie";
-$statement = $db->prepare($sql);
-$statement->bindValue(":item", $item, PDO::PARAM_STR);
-$statement->execute();
-
-while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-{
-	echo $row["name"] . ", " . $row["colorName"] . ", " . $row["size"] . "<br />\n";
-}
-
-echo "<br />\n";
-*/
-?>
-
-				</div>
-				<div class="col-md-4">
 
 <?php
-/*
+
 echo "<br />\n<br />\n" . "Comments for user 1:" . "<br />\n<br />\n";
 $sql = "SELECT date, body FROM comment WHERE user_id = :user";
 $user = "1";
@@ -120,7 +72,7 @@ while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 	echo "Body: " . $row["body"] . "<br />\n";
 	echo "<br />\n";
 }
-*/
+
 ?>
 				</div>
 			</div>
